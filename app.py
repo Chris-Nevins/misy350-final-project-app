0e9dbb8ef28511ebc77c986de87ef1c8ba8224a4
Chris <cnevins@udel.edu>
2026-04-01 11:54:58 -0400

    .

--- app.py
+++ app.py
@@ -1,4 +1,28 @@
 import streamlit as st
+from datetime import datetime
+import json
+from pathlib import Path
 
 #st.set_page_config(page_title="Smart Coffee Kiosk Application")
 st.title("Smart Coffee Kiosk Application")
+
+#=============================
+# A. User Registration & Login
+#=============================
+
+#=============================
+# B. Session State Management
+#=============================
+
+#=============================
+# C. Role-Based Routing
+#=============================
+
+#=============================
+# D. JSON-Based Data Storage
+#=============================
+
+#=============================
+# E. CRUD System
+#=============================
+
