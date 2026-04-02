de6081b5aaac2ede52a6fb1605ec39b77a691b8b
Chris <cnevins@udel.edu>
2026-04-02 15:59:47 -0400

    additions

--- app.py
+++ app.py
@@ -2,14 +2,77 @@ import streamlit as st
 from datetime import datetime
 import json
 from pathlib import Path
+import uuid
+import time
 
 #st.set_page_config(page_title="Smart Coffee Kiosk Application")
-st.title("Smart Coffee Kiosk Application")
+st.title("Precision Hardware")
 
 #=============================
 # A. User Registration & Login
 #=============================
 
+users = []
+
+json_path = Path("users.json")
+if json_path.exists():
+    with open(json_path, "r") as f:
+        users = json.load(f)
+
+# Login
+st.subheader("Log In")
+with st.container(border=True):
+    email_input = st.text_input("Email", key="email_login")
+    password_input = st.text_input("Password", type="password", key="password_login")
+    
+    if st.button("Log In", type="primary", use_container_width=True):
+        with st.spinner("Logging in..."):
+            time.sleep(2) # Fake backend delay
+            
+            # Find user
+            found_user = None
+            for user in users:
+                if user["email"].strip().lower() == email_input.strip().lower() and user["password"] == password_input:
+                    found_user = user
+                    break
+
+            if found_user:
+                st.success(f"Welcome back, {found_user['email']}!")
+                st.session_state["logged_in"] = True
+                st.session_state["user"] = found_user
+                st.session_state["role"] = found_user["role"]
+                st.session_state["page"] = "home"
+
+                time.sleep(2)
+                st.rerun()
+            else:
+                st.error("Invalid credentials")
+
+# Registration
+st.subheader("New Instructor Account")
+with st.container(border=True):
+    new_email = st.text_input("Email", key="email_register")
+    new_password = st.text_input("Password", type="password", key="password_edit")
+    
+    if st.button("Create Account", key= "register_btn"):
+        with st.spinner("Creating account..."):
+            time.sleep(2) # Fake backend delay
+            # ... (Assume validation logic here) ...
+            users.append({
+                "id": str(uuid.uuid4()),
+                "email": new_email,
+                "password": new_password,
+                "role": "Instructor"
+            })
+
+            with open(json_path, "w") as f:
+                json.dump(users, f)
+
+            st.success("Account created!")
+            st.rerun()
+
+st.write("---")
+st.dataframe(users)
 #=============================
 # B. Session State Management
 #=============================
